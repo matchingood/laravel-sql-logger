85a8d3d78a6aaa279dc5e8d7e320ad3f2bfbc15a
khouki <khouki@users.noreply.github.com>
2018-09-21 18:44:49 +0900

    feat: バックトレース出力を実装

--- src/Writer.php
+++ src/Writer.php
@@ -134,13 +134,23 @@ class Writer
     private function toSlack($context)
     {
         $record['context'] = $context;
-        $record['extra'] = [];
+
+        $backTrace = '未実装';
+        if (method_exists('Util', 'getFormattedBackTrace')) {
+            $backTrace = Util::getFormattedBackTrace();
+        }
+
+        $content  = "*database* {$record['context']['database']}\n";
+        $content .= "*query* ({$record['context']['time']})\n{$record['context']['query']}\n";
+        $content .= "{$record['context']['request']}\n";
+        $content .= "*backTrace*\n{$backTrace}\n";
 
         $notification = (new SlowQuerySlackNotification())
             ->setLevel(SlowQuerySlackNotification::ERROR)
             ->setIsAnnounced(false)
             ->setAttachmentTitle(self::SLOW_QUERY_SLACK_TITLE)
-            ->setFields(array_merge($record['context'], $record['extra']));
+            ->setFields([$content]);
+
         if (!empty($notification->routeNotificationForSlack())) {
             $notification->notify(new SlackPosted);
             return;
